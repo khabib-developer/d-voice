f21021c450b22011c11913ff130938f2bd11313f
Habib <habib@MacBook-Pro-Habib.local>
2025-06-03 14:57:14 +0500

    commit

--- types/shaka-player.d.ts
+++
@@ -1,9 +0,0 @@
-// types/shaka-player.d.ts
-declare module "shaka-player" {
-  const shaka: any;
-  export default shaka;
-}
-declare module "shaka-player/dist/shaka-player.compiled" {
-  const shaka: any;
-  export default shaka;
-}
